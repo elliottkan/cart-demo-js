16a1c43199162ef8715ad7e0695a256403a1c4e9
Elliott Kan <elliot.kan@convertdigital.com.au>
2023-04-15 22:53:05 +1000

    Minicart is now dynamic - can now add items

--- src/app.js
+++ src/app.js
@@ -43,11 +43,85 @@ function generateShop() {
 
 generateShop();
 
-function generateMiniCart() {
+function generateMiniCartItems() {
+  const miniCartInner = document.querySelector('[data-mini-cart-inner]');
+
+  cart.forEach((cartItem)=>{
+    shopItems.forEach((shopItem)=>{
+      let cartItemNotYetRendered = miniCartInner.querySelector(`[data-mini-cart-item-id="${cartItem.id}"]`) ? false : true;
+      // If the item is not in the cart, add it
+      if(shopItem.id == cartItem.id && cartItemNotYetRendered){
+        let cartItemHTML = document.createElement('li');
+        let {
+          id = cartItem.id,
+          name = shopItem.name,
+          price = parseInt(shopItem.price * cartItem.itemQuantity),
+          quantity = cartItem.itemQuantity,
+          img = shopItem.img,
+          alt = shopItem.alt
+        } = {}
+        cartItemHTML.innerHTML = `
+        <li data-mini-cart-item-id=${id} class="flex py-6">
+          <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+            <img src="${img}" alt="${alt}" class="h-full w-full object-cover object-center">
+          </div>
   
+          <div class="ml-4 flex flex-1 flex-col">
+            <div>
+              <div class="flex justify-between text-base font-medium text-gray-900">
+                <h3>
+                  <a href="#">${name}</a>
+                </h3>
+                <p data-minicart-price class="ml-4">$${price}</p>
+              </div>
+            </div>
+            <div class="flex flex-1 items-end justify-between text-sm">
+              <p data-minicart-quantity class="text-gray-500">Qty ${quantity}</p>
+  
+              <div class="flex">
+                <button onclick="removeItem(${id})" type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li>
+        `
+        miniCartInner.appendChild(cartItemHTML)
+      }
+      // If the item is already in the cart, update the quantity
+      if (shopItem.id == cartItem.id && !cartItemNotYetRendered) {
+        updateMiniCartItemQuantity(cartItem);
+        updateMiniCartItemPricing(shopItem, cartItem);
+      }
+      updateMiniCartFinalPrice();
+    })
+  })
 }
 
-generateCart()
+function updateMiniCartItemQuantity(cartItem) {
+  let cartItemHTML = document.querySelector(`[data-mini-cart-item-id="${cartItem.id}"]`);
+  let cartItemQuantity = cartItemHTML.querySelector("[data-minicart-quantity]");
+  cartItemQuantity.innerHTML = `Qty ${cartItem.itemQuantity}`;
+}
+
+function updateMiniCartItemPricing(shopItem, cartItem) {
+  let cartItemHTML = document.querySelector(`[data-mini-cart-item-id="${cartItem.id}"]`);
+  let cartItemPrice = cartItemHTML.querySelector("[data-minicart-price]");
+  let price = parseInt(shopItem.price * cartItem.itemQuantity);
+  cartItemPrice.innerHTML = `$${price}`;
+}
+
+function updateMiniCartFinalPrice() {
+  let miniCartFinalPrice = document.querySelector("[data-mini-cart-final-price]");
+  let cartTotal = 0;
+  cart.forEach((cartItem) => {
+    shopItems.forEach((shopItem) => {
+      if (shopItem.id == cartItem.id) {
+        cartTotal += parseInt(shopItem.price * cartItem.itemQuantity);
+      }
+    });
+  });
+  miniCartFinalPrice.innerHTML = `$${cartTotal}`;
+}
 
 function increment(id, stock) {
     let itemQuantityContainer = document.querySelector(`[data-item-quantity-${id}]`);
@@ -95,7 +169,12 @@ function updateStock(id, itemQuantity) {
   };
 }
 
+function removeItem(id) {
+
+}
+
 function openModal() {
+  generateMiniCartItems()
   const modal = document.querySelector("[data-mini-cart-slideout]");
   modal.classList.remove("hidden");
   modal.addEventListener(
--- src/product.js
+++ src/product.js
@@ -1,6 +1,6 @@
 let shopItems = [
     {
-      id: "1001",
+      id: 1001,
       name: "Zach's Ankle Supporters",
       price: 145.50,
       stock: 2,
@@ -9,7 +9,7 @@ let shopItems = [
       img: "https://images.unsplash.com/photo-1631287381310-925554130169?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2050&q=80",
     },
     {
-      id: "1002",
+      id: 1002,
       name: "Shawn's Steeze",
       price: 1000,
       stock: 15,
@@ -18,7 +18,7 @@ let shopItems = [
       img: "https://images.unsplash.com/photo-1597639564819-8fa17aa752db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
     },
     {
-      id: "1003",
+      id: 1003,
       name: "Jesse's Hustle Bible",
       price: 69.99,
       stock: 1000,
@@ -27,7 +27,7 @@ let shopItems = [
       img: "https://images.unsplash.com/photo-1600019246742-3b66977db044?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
     },
     {
-      id: "1004",
+      id: 1004,
       name: "Blake's Cauldron",
       price: 12800,
       stock: 50,
